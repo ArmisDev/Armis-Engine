61b4c3cfb400dfa9680f169c2750f86d772b038f
ArmisDev <113211223+ArmisDev@users.noreply.github.com>
2024-01-24 14:46:20 -0600

    Window Commit
    Update to add window display

---
+++ ArmisEngine/ArmisEngine/AppWindow.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Window.h"
+class AppWindow : public Window
+{
+public:
+	AppWindow();
+	~AppWindow();
+
+	// Inherited via Window
+	void onCreate() override;
+	void onUpdate() override;
+	void onDestroy() override;
+};
